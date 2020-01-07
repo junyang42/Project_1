54fb1d54f2f85036e89b8b92a30939e8c82d4f95
junyang42 <57102713+junyang42@users.noreply.github.com>
2020-01-06 18:14:11 -0600

    Update random.py

--- random.py
+++ random.py
@@ -1,7 +1,3 @@
 print("Hello")
-<<<<<<< HEAD
 print("Am I doing this correctly?")
 print("Let's see if you see this new change.")
-=======
-print("Hello too!")
->>>>>>> 4e95b7b877af8dc60e26de646ace59da4de43de6

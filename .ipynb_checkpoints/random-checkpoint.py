9617ae8e4944c029188084f88ddfd3b814624874
ArthurC <arthurcw@gmail.com>
2020-01-11 20:32:09 -0600

    cleaning folder

--- .ipynb_checkpoints/random-checkpoint.py
+++
@@ -1,7 +0,0 @@
-print("Hello")
-<<<<<<< HEAD
-print("Am I doing this correctly?")
-print("Let's see if you seess asd awrwtwr523t this new change.")
-=======
-print("Hello too!")
->>>>>>> 4e95b7b877af8dc60e26de646ace59da4de43de6
